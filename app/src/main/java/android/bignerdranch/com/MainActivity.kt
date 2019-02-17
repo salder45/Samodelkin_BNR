4c9158af5c3d56333c2bdacc8f86285a56ab608b
Eder Martinez <edermtz.04@gmail.com>
2019-02-17 14:47:36 -0600

    Chapter 21 -> wiring up views

--- app/src/main/java/android/bignerdranch/com/MainActivity.kt
+++ app/src/main/java/android/bignerdranch/com/MainActivity.kt
@@ -2,11 +2,31 @@ package android.bignerdranch.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var characterData = CharacterGenerator.generate()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //wiring up views
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val raceTextView = findViewById<TextView>(R.id.raceTextView)
+        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
+        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
+        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
+
+        val generateButton = findViewById<Button>(R.id.generateButton)
+        //
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
     }
 }
